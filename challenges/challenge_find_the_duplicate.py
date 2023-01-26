9fe8a4725a5f1e24cd7c130ade872b54261cb8cf
klauslube <klauslube93@gmail.com>
2023-01-26 15:55:51 -0300

    feat: challenge 5 - find duplicate values

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    if len(nums) <= 1:
+        return False
+
+    for index in range(len(nums) - 1):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+
+        elif nums[index] == nums[index + 1]:
+            return nums[index]
+    return False
