640a1dc350d67e0ae1187cffb6803e3403feca16
klauslube <klauslube93@gmail.com>
2023-01-26 11:03:30 -0300

    feat: challenger one

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for time in permanence_period:
+        if not isinstance(time[0], int) or not isinstance(time[1], int):
+            return None
+        if time[0] <= target_time <= time[1]:
+            count += 1
+
+    return count
