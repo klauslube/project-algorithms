60c75ca8f71f048223fba9e0958cbf5f25562096
klauslube <klauslube93@gmail.com>
2023-01-26 11:38:30 -0300

    test: challenge 02 - encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('abcde', 2) == 'edc_ba'
+    assert encrypt_message('abcde', 3) == 'cde_ba'
+    assert encrypt_message('abcde', 6) == 'edcba'
+
+    with pytest.raises(TypeError, match="invalid key type"):
+        encrypt_message('abc', 'abc')
+
+    with pytest.raises(TypeError, match="invalid message type"):
+        encrypt_message(1, 9)
