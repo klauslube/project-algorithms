35d98f0924df36238655abd8659225a4c938675e
klauslube <klauslube93@gmail.com>
2023-01-26 12:10:48 -0300

    feat: challenge 3 - recursive palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    elif word[low_index] != word[high_index]:
+        return False
+
+    elif low_index >= high_index:
+        return True
+
+    else:
+        if low_index <= high_index:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
